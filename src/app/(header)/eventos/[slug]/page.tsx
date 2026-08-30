58ba25066939320912939ac29d132033787dbdf3
Yadira-rs <ing.santos05iris@gmail.com>
2026-08-30 03:31:58 -0600

    Delete src/app/(header)/eventos/[slug]/page.tsx

--- src/app/(header)/eventos/[slug]/page.tsx
+++
@@ -1,4 +0,0 @@
-export default async function DetalleEventoPage({ params }: PageProps<"/eventos/[slug]">) {
-  const { slug } = await params;
-  return <div className="p-8">/eventos/{slug} — DetalleEventoPage placeholder</div>;
-}
