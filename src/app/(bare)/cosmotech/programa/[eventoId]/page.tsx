f9b7f94dc647d57749901a397e09229e530d0020
Yadira-rs <ing.santos05iris@gmail.com>
2026-08-30 03:30:43 -0600

    Delete src/app/(bare)/cosmotech/programa/[eventoId]/page.tsx

--- src/app/(bare)/cosmotech/programa/[eventoId]/page.tsx
+++
@@ -1,4 +0,0 @@
-export default async function ProgramaConferencia({ params }: PageProps<"/cosmotech/programa/[eventoId]">) {
-  const { eventoId } = await params;
-  return <div className="p-8">/cosmotech/programa/{eventoId} — ProgramaConferencia placeholder</div>;
-}
