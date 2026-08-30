7c0027febaa7bc8465876aa69006c905f2ffba7d
Yadira-rs <ing.santos05iris@gmail.com>
2026-08-30 03:30:23 -0600

    Delete src/app/(auth)/layout.tsx

--- src/app/(auth)/layout.tsx
+++
@@ -1,7 +0,0 @@
-export default function AuthLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <main className="flex flex-1 items-center justify-center bg-neutral px-4 py-16">
-      {children}
-    </main>
-  );
-}
