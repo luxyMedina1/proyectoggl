06f4ed7b936b0edff0aaa98af280b411084a17cd
Yadira-rs <ing.santos05iris@gmail.com>
2026-08-30 03:34:07 -0600

    Elimina src/app/providers.tsx del App Router previo

--- src/app/providers.tsx
+++
@@ -1,9 +0,0 @@
-'use client';
-
-import { StoreProvider } from '@/lib/store/StoreProvider';
-
-// TODO(auth-phase): mount <Loader/> / <AuthModalProvider> / <ToastContainer/> here,
-// same position they occupy around the router <Outlet/> in v2's App.tsx.
-export function Providers({ children }: { children: React.ReactNode }) {
-    return <StoreProvider>{children}</StoreProvider>;
-}
