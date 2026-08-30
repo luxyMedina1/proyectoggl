caba0f36ddf2fd98f4e8d7d6b922b2b205f45270
Yadira-rs <ing.santos05iris@gmail.com>
2026-08-30 03:31:50 -0600

    Delete src/app/(header)/citypass/terminar_compra/[compraId]/page.tsx

--- src/app/(header)/citypass/terminar_compra/[compraId]/page.tsx
+++
@@ -1,6 +0,0 @@
-export default async function CityPassTerminarCompra({
-  params,
-}: PageProps<"/citypass/terminar_compra/[compraId]">) {
-  const { compraId } = await params;
-  return <div className="p-8">/citypass/terminar_compra/{compraId} — CityPassTerminarCompra placeholder</div>;
-}
