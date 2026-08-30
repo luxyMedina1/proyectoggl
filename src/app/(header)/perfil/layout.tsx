a0bf9947b4d0a8c4cbaa2b43a8068fb9ae2e03e0
Yadira-rs <ing.santos05iris@gmail.com>
2026-08-30 03:32:35 -0600

    Elimina el layout placeholder de /perfil del App Router previo (src/app)

--- src/app/(header)/perfil/layout.tsx
+++
@@ -1,5 +0,0 @@
-import { RequireAuth } from "@/components/auth/RequireAuth";
-
-export default function PerfilLayout({ children }: LayoutProps<"/perfil">) {
-  return <RequireAuth>{children}</RequireAuth>;
-}
