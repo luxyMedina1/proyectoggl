ab44a9fc0748a104fa9bda2d829cee2641ad899b
Yadira-rs <ing.santos05iris@gmail.com>
2026-08-30 03:25:06 -0600

    Pasa config y colores iniciales del servidor a los providers (sin salto de marca)

--- app/providers.tsx
+++ app/providers.tsx
@@ -4,16 +4,27 @@ import { ReactNode } from "react";
 import { Provider } from "react-redux";
 import { store } from "../store/store";
 import { ColorConfigProvider } from "../context/ColorContext";
+import type { ConfigResponse } from "../context/ColorContext";
+import type { BrandColors } from "@/lib/config/getSiteConfig";
 import { AuthModalProvider } from "../context/AuthModalContext";
 
-export const Providers = ({ children }: { children: ReactNode }) => {
-    return (
-        <Provider store={store}>
-            <ColorConfigProvider>
-                <AuthModalProvider>
-                    {children}
-                </AuthModalProvider>
-            </ColorConfigProvider>
-        </Provider>
-    );
+export const Providers = ({
+  children,
+  configInicial,
+  coloresIniciales,
+}: {
+  children: ReactNode;
+  configInicial: ConfigResponse | null;
+  coloresIniciales: BrandColors;
+}) => {
+  return (
+    <Provider store={store}>
+      <ColorConfigProvider
+        configInicial={configInicial}
+        coloresIniciales={coloresIniciales}
+      >
+        <AuthModalProvider>{children}</AuthModalProvider>
+      </ColorConfigProvider>
+    </Provider>
+  );
 };
