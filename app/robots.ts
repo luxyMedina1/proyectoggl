8e682ca7f4548c2795023ba700635dc58e5e29c5
RIADWaffle <106703366+RIADWaffle@users.noreply.github.com>
2026-09-09 10:09:24 -0600

    Update robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -19,6 +19,6 @@ export default function robots(): MetadataRoute.Robots {
         "/citypass/terminar_compra/",
       ],
     },
-    sitemap: `${SITE_URL}/sitemap.xml`,
+    sitemap: `${SITE_URL}/sitemap/0.xml`,
   };
 }
